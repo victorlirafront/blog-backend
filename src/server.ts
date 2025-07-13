1a71b59ed303577fe316a74d769851634d686242
victorlirafront <victorliradeveloper@gmail.com>
2025-07-13 14:51:23 -0300

    feat: add new routes

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import emailRoutes from './routes/emailRoutes';
+import getProducts from './routes/getProducts';
+import createPost from './routes/createPost';
+import searchPost from "./routes/searchPost"
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
+app.use(express.json());
+
+app.use('/api/', emailRoutes);
+app.use('/api/', getProducts);
+app.use('/api/', createPost);
+app.use('/api/', searchPost);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
