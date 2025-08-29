da6af19970dd9d1756d4472f41fdccbdf164b5f1
victorlirafront <victorliradeveloper@gmail.com>
2025-08-28 21:24:15 -0300

    test: testing get post endpoint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "The blog API is running"', () => {
+      expect(appController.getHello()).toBe('The blog API is running');
     });
   });
 });
