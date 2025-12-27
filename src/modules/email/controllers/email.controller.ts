2cd97f5ce3594d8f98a9cd588786da6894e65876
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-12-27 10:07:46 -0300

    feat(email): add pipe to transform email data

--- src/modules/email/controllers/email.controller.ts
+++ src/modules/email/controllers/email.controller.ts
@@ -2,13 +2,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { EmailService } from '../services/email.service';
 import { SendEmailDto } from '../dto/send-email.dto';
 import { EmailResponse } from '../views/email.response';
+import { SendEmailTransformPipe } from '../pipes/send-email-transform.pipe';
 
 @Controller()
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post('sendEmail')
-  async sendEmail(@Body() data: SendEmailDto): Promise<EmailResponse> {
+  async sendEmail(
+    @Body(SendEmailTransformPipe) data: SendEmailDto,
+  ): Promise<EmailResponse> {
     await this.emailService.sendEmail(data);
     return new EmailResponse('Email enviado com sucesso');
   }
